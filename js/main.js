ab3ec0ee323867561bff66eb8db7ae2d327175a8
Ahmadsabili <ahmadsabili0081@gmail.com>
2022-09-23 15:03:43 +0700

    initial commit

--- js/main.js
+++ js/main.js
@@ -2,19 +2,17 @@ let text__area = document.querySelector('.text__area');
 let infoCharacter = document.querySelector('.infoCharacter');
 let btnAdd = document.querySelector('.addNote');
 let inputTitle = document.querySelector('.inputTitle');
+let formSubmit = document.querySelector('.form');
+let elementSave = null;
 let wrapper__result = document.querySelector('.wrapper__result');
-let ediEls = false;
-let editBtn = document.querySelector('.editBtn')
-text__area.addEventListener('keydown',() => {
-  let hasil = text__area.value;
-  let angka = 1000
-  infoCharacter.innerHTML = hasil.length + ` / ${angka} charackters`;
+let getIdEls;
+window.addEventListener('DOMContentLoaded',() => {
+  let resultTheObj = storeLocal.getNotesLocal();
+  resultTheObj.forEach((itemObj) => {
+    resultObj.createEls(itemObj)
+  })
 });
 
-btnAdd.addEventListener('click',(e) => {
-  e.preventDefault();
-  showElement.createEl(e)
-});
 class createObj{
   constructor(title,textArea,id,timeFull,dateFull){
     this.id = id
@@ -24,40 +22,45 @@ class createObj{
     this.dateFull = dateFull;
   }
 }
-window.addEventListener('DOMContentLoaded',() => {
-  let resultTheObj = storeLocal.getNotesLocal();
-  resultTheObj.forEach((itemObj) => {
-    resultObj.createEls(itemObj)
-  })
+text__area.addEventListener('keydown',() => {
+  let hasil = text__area.value;
+  let angka = 1000
+  infoCharacter.innerHTML = hasil.length + ` / ${angka} charackters`;
 });
-class showElement{
-  static createEl(data){
-    let emptyValue = inputTitle.value;
-    let text__areaEmpty = text__area.value;
+
+formSubmit.addEventListener('submit',(e) => {
+  e.preventDefault();
+  let emptyValue = inputTitle.value;
+  let text__areaEmpty = text__area.value;
+  if(!emptyValue || !text__areaEmpty){
+    window.alert('Mohon isi form, jangan kosong!!')
+  }else{
     // date now
     let dates = new Date()
     let day = dates.getDate() ;
     let month = ("0" + (dates.getMonth() + 1));
     let year = dates.getFullYear();
-    // date time
+    // date timev
     let seconds = dates.getSeconds() < 10 ? "0" + dates.getSeconds() : dates.getSeconds();
     let minutes = dates.getMinutes() < 10 ? "0" + dates.getMinutes() : dates.getMinutes();
     let hours = dates.getHours() < 10 ? "0" + dates.getHours() : dates.getHours(); 
     let dateFull = `${day}/${month}/${year}`
     let timeFull = `${hours}:${minutes}:${seconds}`;
-    if(!emptyValue || !text__areaEmpty){
-      window.alert('Mohon isi form, jangan kosong!!')
-    }else{
-       let getId = () => {
-        return Math.floor(Math.random() * 1000);
-       }
-        let objResult = new createObj(inputTitle.value,text__area.value,getId(),timeFull,dateFull);
-        storeLocal.saveNotesLocal(objResult);
-        resultObj.clearInput();
-        resultObj.createEls(objResult)
+    let getId = () => {
+      return Math.floor(Math.random() * 1000);
+    }
+    let objResult = new createObj(inputTitle.value,text__area.value,getId(),timeFull,dateFull);
+    if(elementSave === null){
+        resultObj.createEls(objResult);
+        storeLocal.saveNotesLocal(objResult)
+        inputTitle.value = "";
+        text__area.value = "";
+     }else{
+        resultObj.editNotes(objResult)
+        elementSave = null;
     }
   }
-}
+});
 wrapper__result.addEventListener('click',(e) => {
   resultObj.deleteResult(e);
   resultObj.editResult(e);
@@ -90,7 +93,6 @@ class resultObj{
   `
   wrapper__result.appendChild(resultNotes);
 }
-
   static deleteResult(data){
       if(data.target.classList.contains('delete')){
         let targetRemove = data.target.parentElement.parentElement;
@@ -100,45 +102,30 @@ class resultObj{
   }
   static editResult(e){
     if(e.target.classList.contains('edit')){
-      let  targetId = e.target.parentElement.parentElement;
-      let currentValueTitle = targetId.childNodes[1].childNodes[3].textContent
-      let currentValueContent = targetId.childNodes[3].textContent;
-      inputTitle.value = currentValueTitle;
-      text__area.value = currentValueContent;
-      ediEls = true;
-      btnAdd.style.display = "none";
-      editBtn.style.display = "block";
-      editBtn.addEventListener('click',(e) => {
-        e.preventDefault();
-        if(ediEls && targetId){
-          let valueNew = inputTitle.value;
-          let contentNew = text__area.value;
-          let getLocal = storeLocal.getNotesLocal();
-          let valueNewObj =  getLocal.map((itemnew) => {
-            if(itemnew.id === parseInt(targetId.id)){
-              itemnew.title = valueNew;
-              itemnew.content = contentNew;
-            }
-            return itemnew
-          });
-          localStorage.setItem("notes",JSON.stringify(valueNewObj))
-          targetId.children[0].children[1].innerHTML = valueNew;
-          targetId.children[1].innerHTML = contentNew;
-          location.reload();
-          window.alert('Your list hasbeen update')
-          resetEl();
-          return
-        }
-      })
-    }
+        elementSave = e.target.parentElement.parentElement;
+        getIdEls = parseInt(elementSave.id)
+        inputTitle.value = elementSave.children[0].children[1].textContent;
+        text__area.value = elementSave.children[1].textContent;
+        btnAdd.innerHTML = "Edit";
+    }else null
   }
-}
-function resetEl(){
-  inputTitle.value = ""
-  text__area.value = ""
-  btnAdd.style.display = "block";
-  editBtn.style.display = "none";
-  ediEls = false;
+  static editNotes(){
+    let getObj = storeLocal.getNotesLocal();
+    let updateObj =  getObj.map((itemUpdate) => {
+      if(itemUpdate.id === getIdEls){
+        itemUpdate.title = inputTitle.value;
+        itemUpdate.content = text__area.value;
+      }
+      return itemUpdate
+    });
+
+    localStorage.setItem('notes',JSON.stringify(updateObj))
+    elementSave.children[0].children[1].innerHTML = inputTitle.value;
+    elementSave.children[1].innerHTML = text__area.value;
+    btnAdd.innerHTML = "Add Note"
+    inputTitle.value = "";
+    text__area.value = "";
+  }
 }
 class storeLocal{
   static saveNotesLocal(data){
@@ -168,7 +155,10 @@ class storeLocal{
       notes = JSON.parse(localStorage.getItem('notes'));
     }
     let targetIndex = data.children[0].children[1].innerText;
-    notes.splice(notes.indexOf(targetIndex),1);
-    localStorage.setItem("notes",JSON.stringify(notes));
+    let hasil =  notes.findIndex((itemIndex) => {
+          return itemIndex.title === targetIndex
+    });
+    notes.splice(hasil,1);
+    localStorage.setItem('notes',JSON.stringify(notes))
   }
 }
